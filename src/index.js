fb302219b7b7a33573c637f6e21e3aed6f6283ff
Adrian Lacheta <46226061+adiczq@users.noreply.github.com>
2023-03-30 19:53:53 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const price = document.querySelector("#price");
+const people = document.querySelector("#people");
+const tip = document.querySelector("#tip");
+const btn = document.querySelector(".count");
+const error = document.querySelector(".error");
+const costInfo = document.querySelector(".cost-info");
+const cost = document.querySelector(".cost");
+
+const check = () => {
+  if (price.value == "" || people.value == "" || tip.value == 0) {
+    error.textContent = "Uzupełnij wszystkie pola!";
+    costInfo.style.display = "none";
+  } else {
+    error.textContent = "";
+    countBill();
+  }
+};
+
+const countBill = () => {
+  const newPrice = parseFloat(price.value);
+  const newPeople = parseInt(people.value);
+  const newTip = parseFloat(tip.value);
+
+  const sum = (newPrice + newPrice * newTip) / newPeople;
+  costInfo.style.display = "block";
+  console.log(sum);
+  cost.textContent = sum.toFixed(2);
+};
+
+btn.addEventListener("click", check);

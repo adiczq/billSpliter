b6c9749991f91d38f94a4d88bfbd1dc1bce11a67
Adrian Lacheta <46226061+adiczq@users.noreply.github.com>
2023-03-30 20:11:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const btn = document.querySelector(".count");
 const error = document.querySelector(".error");
 const costInfo = document.querySelector(".cost-info");
 const cost = document.querySelector(".cost");
+const costTip = document.querySelector(".costTip");
 
 const check = () => {
   if (price.value == "" || people.value == "" || tip.value == 0) {
@@ -20,11 +21,12 @@ const countBill = () => {
   const newPrice = parseFloat(price.value);
   const newPeople = parseInt(people.value);
   const newTip = parseFloat(tip.value);
-
+  const total = newPrice + newPrice * newTip;
   const sum = (newPrice + newPrice * newTip) / newPeople;
   costInfo.style.display = "block";
   console.log(sum);
   cost.textContent = sum.toFixed(2);
+  costTip.textContent = total.toFixed(2);
 };
 
 btn.addEventListener("click", check);
